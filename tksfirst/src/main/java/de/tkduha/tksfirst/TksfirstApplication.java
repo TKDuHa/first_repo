87ff8fd30a3f467321dbcfe282fb16a32743623c
Tim Kruppa <tkruppa@ab-data.de>
2020-09-09 10:51:56 +0200

    Init commit

---
+++ tksfirst/src/main/java/de/tkduha/tksfirst/TksfirstApplication.java
@@ -0,0 +1,13 @@
+package de.tkduha.tksfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TksfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TksfirstApplication.class, args);
+	}
+
+}
---
+++ tksfirst/src/test/java/de/tkduha/tksfirst/TksfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tkduha.tksfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TksfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
