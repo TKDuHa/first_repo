84dc4a5c9021a8f4dd640c35d1afdf1fbabd7983
Tim Kruppa <tkruppa@ab-data.de>
2020-09-10 11:38:51 +0200

    Implemented for Oracle, regarding sequence instead of identity

--- tksfirst/src/main/java/de/tkduha/tksfirst/entities/Animal.java
+++ tksfirst/src/main/java/de/tkduha/tksfirst/entities/Animal.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 public class Animal {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer id;
 	
 	@NotBlank
