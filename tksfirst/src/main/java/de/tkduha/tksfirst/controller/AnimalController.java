3fb17ed8476a22416a5943a1b496006639f905a9
Tim Kruppa <tkruppa@ab-data.de>
2020-09-21 15:45:03 +0200

    Put, Delete ergänzt, Post, Get korrigiert

--- tksfirst/src/main/java/de/tkduha/tksfirst/controller/AnimalController.java
+++ tksfirst/src/main/java/de/tkduha/tksfirst/controller/AnimalController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,15 +23,33 @@ public class AnimalController {
 	
 	@GetMapping("/animal")
 	public List<Animal> getAllAnimals() {
-		return animalRepository.findAll();
+		return animalRepository.findAllByOrderByIdAsc();
 	}
 	
 	@PostMapping("/animal")
 	public ResponseEntity<Animal> addAnimal(@RequestBody Animal animal) {
 		if(animalRepository.existsById(animal.getId())) {
-			return new ResponseEntity<Animal>(animalRepository.getOne(animal.getId()), HttpStatus.METHOD_NOT_ALLOWED);
+			return new ResponseEntity<Animal>(animalRepository.findById(animal.getId()).get(), HttpStatus.METHOD_NOT_ALLOWED);
 		}
+		animalRepository.save(animal);
 		return new ResponseEntity<Animal>(animal, HttpStatus.CREATED);
 	}
 	
+	@PutMapping("/animal")
+	public ResponseEntity<Animal> modifyAnimal(@RequestBody Animal animal) {
+		if(!animalRepository.existsById(animal.getId())) {
+			return new ResponseEntity<Animal>(animal, HttpStatus.NOT_FOUND);
+		}
+		animalRepository.save(animal);
+		return new ResponseEntity<Animal>(animal, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/animal")
+	public ResponseEntity<Animal> deleteAnimal(@RequestBody Animal animal) {
+		if(!animalRepository.existsById(animal.getId())) {
+			return new ResponseEntity<Animal>(animal, HttpStatus.NOT_FOUND);
+		}
+		animalRepository.deleteById(animal.getId());
+		return new ResponseEntity<Animal>(animal, HttpStatus.OK);
+	}
 }
--- tksfirst/src/main/java/de/tkduha/tksfirst/repositories/AnimalRepository.java
+++ tksfirst/src/main/java/de/tkduha/tksfirst/repositories/AnimalRepository.java
@@ -1,9 +1,11 @@
 package de.tkduha.tksfirst.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import de.tkduha.tksfirst.entities.Animal;
 
 public interface AnimalRepository extends JpaRepository<Animal, Integer>{
-
+	public List<Animal> findAllByOrderByIdAsc();
 }
